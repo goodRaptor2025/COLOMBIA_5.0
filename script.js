f2edb2e06b150068d22ff7878b985310cc38322c
ING-JUAN0813 <juand081318@gmail.com>
2026-05-19 23:54:32 +0000

    pagina 5.0

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ function toggleLanguage() {
     const btn = document.querySelector('.lang-btn');
     
     if (currentLang === 'es') {
-        // Ocultar componentes en Español, mostrar en Inglés
         document.querySelectorAll('.lang-es').forEach(el => el.style.display = 'none');
         document.querySelectorAll('.lang-en').forEach(el => el.style.display = 'block');
         
-        // Traducir dinámicamente los elementos de menú e inputs con atributos de datos
         document.querySelectorAll('[data-en]').forEach(el => {
             el.textContent = el.getAttribute('data-en');
         });
@@ -16,7 +14,6 @@ function toggleLanguage() {
         btn.textContent = 'Español';
         currentLang = 'en';
     } else {
-        // Ocultar componentes en Inglés, mostrar en Español
         document.querySelectorAll('.lang-es').forEach(el => el.style.display = 'block');
         document.querySelectorAll('.lang-en').forEach(el => el.style.display = 'none');
         
@@ -29,7 +26,6 @@ function toggleLanguage() {
     }
 }
 
-// Asegurar estado por defecto en español al cargar el navegador
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll('.lang-en').forEach(el => el.style.display = 'none');
 });
